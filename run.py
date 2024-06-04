cd5e1f0f24f97c2f21c978998c2ce1af59560379
Abduweli Ali <149704850+Localabduweli@users.noreply.github.com>
2024-06-04 14:30:04 +0100

    changes to validata function

--- run.py
+++ run.py
@@ -12,39 +12,34 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('love_sandwiches')
 
-sales = SHEET.worksheet('sales')
-
-data = sales.get_all_values()
-
-print(data)
-
 
 def get_sales_data():
     """
-    Get sales data from the user
+    Get sales figures input from the user.
     """
+    print("Please enter sales data from the last market.")
+    print("Data should be six numbers, separated by commas.")
+    print("Example: 10,20,30,40,50,60\n")
 
-    print("please enter sales data from the last market.")
-    print("Data should be 6 numbers, separated by a comma ")
-    print("Example: 10, 20, 30, 40 , 50, 60\n")
-
-    data_str = input(" Enter your data provided: ")
-    
+    data_str = input("Enter your data here: ")
     sales_data = data_str.split(",")
     validate_data(sales_data)
 
 
 def validate_data(values):
     """
-
-    converts string data to interger
-    
+    Inside the try, converts all string values into integers.
+    Raises ValueError if strings cannot be converted into int,
+    or if there aren't exactly 6 values.
     """
     try:
+        [int(value) for value in values]
         if len(values) != 6:
             raise ValueError(
-            f"Exactly 6 values are required too proceed you have provided {len(values)}"
-        )
+                f"Exactly 6 values required, you provided {len(values)}"
+            )
     except ValueError as e:
-        print(f"Invalid data: {e} please try again.\n")
+        print(f"Invalid data: {e}, please try again.\n")
+
+
 get_sales_data()

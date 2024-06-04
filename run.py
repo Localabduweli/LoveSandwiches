cb7a8a5980880451f7dd24b5e65e96b9306176bf
Abduweli Ali <149704850+Localabduweli@users.noreply.github.com>
2024-06-04 12:47:30 +0100

    changes to interface

--- run.py
+++ run.py
@@ -17,3 +17,18 @@ sales = SHEET.worksheet('sales')
 data = sales.get_all_values()
 
 print(data)
+
+
+def get_sales_data():
+    """
+    Get sales data from the user
+    """
+
+    print("please enter sales data from the last market.")
+    print("Data should be 6 numbers, separated by a comma ")
+    print("Example: 10, 20, 30, 40 , 50, 60\n")
+
+    data_str = input(" Enter your data provided: ")
+    print(f"The data provided is {data_str}")
+
+get_sales_data()
